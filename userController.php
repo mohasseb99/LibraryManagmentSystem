ea6d1109c960179baf2d8ac08d1c65301f9f05f9
Madonna Magdy Moussa <madonnamagdymoussa@gmail.com>
2023-06-04 00:41:18 +0300

    Add files via upload

--- userController.php
+++ userController.php
@@ -7,13 +7,35 @@ include_once "helper.php";
 //include_once "helper.php"; 
 
 class UserController{
-    function ShowUserInfo(){
+    function  ShowUserInfo(){
         $Id=$_REQUEST["id"];
+        
         $obj=new user($Id);
         $userViewobj= new userview();
         $userViewobj->showuserInfo($obj);
     }
-}
+
+  
+    function AddUser($id){
+
+        $FullName=$_REQUEST["fullName"];
+        $DOB=$_REQUEST["dateOfBirth"];
+        $userTypeId=$_REQUEST["userTypeId"];
+        $Password=$_REQUEST["password"];
+        $Username=$_REQUEST["userName"];
+        $CityId=$_REQUEST["cityid"];
+        $obj= new user($id);
+        $obj->fullName=$FullName;
+        $obj->dateOfBirth=$DOB;
+        $obj->userTypeId=$userTypeId;
+        $obj->password=$Password;
+        $obj->userName=$Username;
+        $obj->cityid=$CityId;
+        $obj->insertNewRecord();
+    
+    }
+
+    }
 
 if("ShowInfo"==$_REQUEST["command"]){
 
@@ -22,20 +44,7 @@ if("ShowInfo"==$_REQUEST["command"]){
 }
 
 if("add"==$_REQUEST["command"]){
-
-    $FullName=$_REQUEST["fullName"];
-    $DOB=$_REQUEST["dateOfBirth"];
-    $userTypeId=$_REQUEST["userTypeId"];
-    $Password=$_REQUEST["password"];
-    $Username=$_REQUEST["userName"];
-    $CityId=$_REQUEST["cityid"];
-    $obj= new user($id);
-    $obj->fullName=$FullName;
-    $obj->dateOfBirth=$DOB;
-    $obj->userTypeId=$userTypeId;
-    $obj->password=$Password;
-    $obj->userName=$Username;
-    $obj->cityid=$CityId;
-    $obj->insertNewRecord();
+    $objadduser = new UserController();
+    $objadduser->AddUser($id);
 }
 ?>
